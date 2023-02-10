3bbfbc34973674c935381b106ea95c9359f6b256
Strahinja <you@example.com>
2023-02-10 22:07:38 +0100

    izmene

--- online.py
+++ online.py
@@ -5,4 +5,15 @@ sreda=8
 cetvrtak=7
 petak=6
 ukupno=ponedeljak + utorak + sreda + cetvrtak + petak
-print(ukupno)
+prosek=ukupno/8
+
+if prosek<10:
+    print("bad employre")
+elif prosek>10:
+    print("good employre")
+else:
+    print("solid employre")
+
+Ime="Strahinja"
+for letter in Ime:
+    print(letter)
