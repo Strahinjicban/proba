91d0debcf332100836bcbbe149104faf4a6f3c73
Strahinja <you@example.com>
2023-04-01 10:59:48 +0200

    zamena radnog vremena

--- online.py
+++ online.py
@@ -3,13 +3,14 @@ ponedeljak=7
 utorak=8
 sreda=8
 cetvrtak=7
-petak=6
+petak=8
 ukupno=ponedeljak + utorak + sreda + cetvrtak + petak
-prosek=ukupno/8
+prosek=ukupno/5
 
-if prosek<10:
+
+if prosek<7:
     print("bad employre")
-elif prosek>10:
+elif prosek>7:
     print("good employre")
 else:
     print("solid employre")
